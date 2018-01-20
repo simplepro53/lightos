9fddf1a662a10435c75da3f01d897175c17f7767
simplepro53 <35572472+simplepro53@users.noreply.github.com>
2018-01-20 06:07:48 -0600

    Add files via upload
    The first beta build, free source code and a compiled EXE.

---
+++ LightOS/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LightOS")>
+<Assembly: AssemblyDescription("Recreating the Windows 8 Metro UI")>
+<Assembly: AssemblyCompany("simplepro53")>
+<Assembly: AssemblyProduct("LightOS")>
+<Assembly: AssemblyCopyright("Copyright ©  2018. No rights reserved.")>
+<Assembly: AssemblyTrademark("Windows 8 is a trademark of Microsoft. Windows is a trademark of Microsoft. Metro is a part of Windows 8.")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("139cf7da-99dd-40d1-9580-912f312d997d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.1")>
+<Assembly: AssemblyFileVersion("0.0.0.1")>
